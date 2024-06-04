f89d1944154ced1a8ad101f8c0b4cec2ba43195f
JoshuaMorales99 <jmorales@frba.utn.edu.ar>
2024-06-03 23:59:03 -0300

    Subo el módulo 4

---
+++ Modulo 4/CuartoModulo.py
@@ -0,0 +1,39 @@
+# =====================================================================================================
+# 🔶 ¿Que es Deep Learning?
+# =====================================================================================================
+"""
+    El Deep Learning es un subconjunto del Machine Learning. Se utilizan algoritmos de redes neuronales
+    los cuales se encuentran inspirados en las redes neuronales humanas.
+    Los algoritmos del Deep Learning realizan una tarea repetitiva que ayuda a mejorar de mandera
+    gradual el resultado del modelo a traves de Deep Layers. Este se puede usar para:
+    - El analisis avanzado de imagenes.
+    - Deteccion de celulas cancerigenas.
+    - En vehiculos autonomos.
+    - Proteger infraestructuras criticas.
+    Los algoritmos de Deep Learning requieren de mucha capacidad de computo (uno de los principales retos
+    que tiene esta disciplina)
+"""
+# =====================================================================================================
+# 🔶 Actividad propuesta.
+# =====================================================================================================
+"""
+    Con Pandas podes leer archivos formato CSV:
+    ✅  Verdadero. 
+    ❌  Falso.
+
+    No se necesita importar Pandas para utilizar la libreria:
+    ❌ Verdadero.
+    ✅ Falso.
+
+    El alias generico de Pandas en Python es:
+    ❌ np.
+    ❌ mlt.
+    ❌ plt.
+    ✅ pd.
+
+    "El corazon de Pandas es el objeto de dato conocido como:
+    ❌ Listas.
+    ❌ Tuplas.
+    ❌ Diccionarios y conjuntos.
+    ✅ DataFrame.
+"""
