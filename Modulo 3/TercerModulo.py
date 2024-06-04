c1e6a8d320ff9b7a193167bbc538630ea9c02112
JoshuaMorales99 <jmorales@frba.utn.edu.ar>
2024-06-03 23:58:42 -0300

    Subo el módulo 3

---
+++ Modulo 3/TercerModulo.py
@@ -0,0 +1,84 @@
+# =====================================================================================================
+# 🔶 ¿Que es POO?
+# =====================================================================================================
+"""
+    Es un modo o paradigma de programacion que nos permite organizar el codigo pensando el problema
+    como una relacion entre cosas, denominadas objetos. Los objetos se trabajan utilizando las clases.
+    Estas nos permiten agrupar un conjunto de variables y funciones.
+    Con la POO se busca resolver aplicaciones cada vez mas complejas, sin el codigo se vuelva un caos.
+    Ademas, se pretende dar pautas para realizar las cosas de manera que otras personas puedan
+    utilizarlas y adelantar su trabajo, de manera que consigamos que el codigo se pueda reutilizar.
+"""
+# =====================================================================================================
+# 🔶 ¿Como se piensa una POO?
+# =====================================================================================================
+"""
+    Es muy parecido a como lo hariamos en la vida real.
+    Por ejemplo: Pensemos en modelizar un coche para luego hacer un esquema POO.
+    - Elemento principal: Coche.
+    - Caracteristicas: Marca, Modelo, Color, etc.
+    - Funcionalidades: reversa, aparcamiento, etc.
+    Ahora pensemoslo desde un esquema POO:
+    - Elemento principal: Clase.
+    - Caracteristicas: Atributos.
+    - Funcionamiento: Metodos.
+
+    Para poder comentar el anterior esquema de POO facilmente aparecen los denominados diagramas de
+    clases, que ilustran todo segun estandares de la UML (Lenguaje unificado de modelado)
+        +---+---------------+
+        | C |   Coche       |
+        +---+---------------+
+        |   Color           |
+        |   Marca           |
+        |   Modelo          |
+        +-------------------+
+        |   estacionar()    |
+        |   arrancar()      |
+        |   parar()         |
+        +-------------------+
+"""
+# =====================================================================================================
+# 🔶 ¿Que es la Inteligencia Artificial (IA)?
+# =====================================================================================================
+"""
+    La inteligencia artificial (IA) es la capacidad que tienen las maquinas para utilizar algoritmos
+    complejos, aprender de los datos y utilizar lo aprendido en la toma de decisiones.
+    A diferencia de las personas, los dispositivos basados en IA no necesitan descansar. Pueden analizar
+    grandes volumenes de informacion a la vez de una forma totalmente recursiva. Ademas, su proporcion
+    de errores es mucho menor en comparacion con un ser humano.
+    Esta esta conformada por dos disciplinas:
+    - Machine learning.
+    - Deep learning.
+"""
+# =====================================================================================================
+# 🔶 ¿Que es el Machine Learning?
+# =====================================================================================================
+"""
+    El Machine Learning el aprendizaje automatico. Es el desarrollo de modelos que nos permiten
+    identificar, en grandes cantidades de datos, patrones para aplicar y crear un modelo que sea capaz,
+    por si solo, de predecir eventos futuros.
+"""
+# =====================================================================================================
+# 🔶 Actividad propuesta.
+# =====================================================================================================
+"""
+    Las listas pueden modificarse:
+    ✅  Verdadero. 
+    ❌  Falso.
+
+    Las tuplas pueden modificarse:
+    ❌ Verdadero.
+    ✅ Falso.
+
+    Las funciones son un concepto exclusivo de ...:
+    ❌ Python.
+    ❌ Java.
+    ❌ PHP.
+    ✅ No es exclusivo, se aplica a cualquier lenguaje de programacion.
+
+    "Peugeot 208" es un ejemplo de ...:
+    ❌ Clase.
+    ❌ Atributo.
+    ❌ Funcionalidad.
+    ✅ Objeto.
+"""
